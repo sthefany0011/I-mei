42a9ad6c89c124835b5beb0435d30d094c8e58f8
Sthefany Silva <sthefany0011@gmail.com>
2020-12-04 12:31:24 -0300

    Protótio Banco de Dados V.1

---
+++ Protótio Banco de Dados/DER db_i_mei.sql
@@ -0,0 +1,41 @@
+-- create database db_i_mei
+
+-- use db_i_mei
+
+/*create table tema(
+
+	id_tema bigint auto_increment,
+    categoria varchar(30),
+    tipo_venda boolean,
+    descricao varchar(255),
+    
+    primary key(id_tema)
+    );*/
+    
+    
+   /* create table usuario(
+    
+		email varchar(50),
+        nome varchar (50),
+        senha varchar(12),
+        
+        primary key(email)
+        
+        );*/
+        
+    
+    /*create table postagem(
+    
+		id_postagem bigint auto_increment,
+        fk_email varchar(50),
+        fk_tema bigint,
+        titulo varchar(100),
+        campo varchar(1000),
+        link_imagem varchar(200),
+        data_post datetime,
+        
+        primary key(id_postagem),
+        foreign key(fk_email) references usuario(email),
+        foreign key(fk_tema) references tema(id_tema)
+        );*/
+        
