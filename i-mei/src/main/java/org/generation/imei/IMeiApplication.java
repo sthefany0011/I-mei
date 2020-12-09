00f6c26cb8fd75e448d232f3466e705cd5c7545b
Cinthia <cinthia.tengan@gmail.com>
2020-12-09 11:09:26 -0300

    Spring do projeto i-mei

---
+++ i-mei/src/main/java/org/generation/imei/IMeiApplication.java
@@ -0,0 +1,13 @@
+package org.generation.imei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMeiApplication.class, args);
+	}
+
+}
---
+++ i-mei/src/test/java/org/generation/imei/IMeiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.imei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
