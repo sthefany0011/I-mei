12b4506261586da6a0224adf2a25c16fcc37c816
Cinthia <cinthia.tengan@gmail.com>
2021-01-05 22:13:31 -0300

    atualizado com o swagger

---
+++ i-mei/src/main/java/org/generation/imei/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package org.generation.imei.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("generation.org.imei.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("I-mei")
+				.description("API do Projeto Integrador")
+				.version("1.0")
+				.contact(contact())
+				.build();
+	}
+
+	private Contact contact() {
+		return new Contact("Cinthia T., Lucas C., João, Sthéfany e Sérgio", 
+				"https://github.com/sthefany0011/I-mei",
+				"Desenvolvedores Web Java Jr em formação pela Generation Brasil");
+	}
+
+}
