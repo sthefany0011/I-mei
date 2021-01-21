28b13dbd44e7efede2a34f5a05e29a66019aff81
Lucas <lkslucas100@gmail.com>
2021-01-21 18:26:22 -0300

    adicionado as rotas entre as paginas

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CarrosselComponent } from './carrossel/carrossel.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'carrossel', pathMatch: 'full'},
+
+  {path:'carrossel', component: CarrosselComponent},
+  {path:'cadastro', component: CadastroComponent},
+  {path:'sobre', component: SobreComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
