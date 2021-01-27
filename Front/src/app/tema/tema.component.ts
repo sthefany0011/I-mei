bba391a31ae8f94d5689aa0be721425e83b37555
João Paulo Oliveira <joaolhplira@gmail.com>
2021-01-27 11:31:34 -0300

    Implementação do get e post de temas

--- Front/src/app/tema/tema.component.ts
+++ Front/src/app/tema/tema.component.ts
@@ -24,12 +24,21 @@ export class TemaComponent implements OnInit {
       alert('sua sessão expirou, faça login novamente.')
       this.router.navigate(['/login'])
     }
+
+    this.finAllTemas() // toda vez que inicia a page tema o método é chamado!
+  }
+
+  finAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listaTemas = resp
+    })
   }
 
   cadastrar(){
     this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
       this.tema = resp
       alert('Tema cadastrado com sucesso!')
+      this.finAllTemas() // toda vez que inserir um tema, será atualizado com este método
       this.tema = new Tema()
     })
     
