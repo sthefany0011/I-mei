bda2f58a1b107a553244a099c08c61e88c24bd81
João Paulo Oliveira <joaolhplira@gmail.com>
2021-02-03 16:15:08 -0300

    Atualização do component inicio

--- i-mei/src/main/java/org/generation/imei/seguranca/BasicSecurityConfig.java
+++ i-mei/src/main/java/org/generation/imei/seguranca/BasicSecurityConfig.java
@@ -34,6 +34,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/usuario/cadastrar").permitAll()
 		.antMatchers("/usuario/{id}").permitAll()
 		.antMatchers("/usuario/atualizar").permitAll()
+		.antMatchers("/usuario/postagem/**").permitAll()
 		.anyRequest().authenticated().and().httpBasic().and().sessionManagement()
 		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
 		// para liberar endpoints dentro do controller para o cliente ter acesso a ele
